ff4be82ae5c9a675c0098912c308b5cdf5d0fecf
Sumit Shende <sumitshende535@gmail.com>
2025-01-19 00:53:27 +0530

    services created

---
+++ library-managment-system/src/com/libraryManagmentSystem/Services.java
@@ -0,0 +1,23 @@
+package com.libraryManagmentSystem;
+
+import java.util.LinkedList;
+
+public class Services {
+    private LinkedList<Book> booksList = new LinkedList<>();
+
+    public void addBook(Book book){
+        booksList.add(book);
+    }
+
+    public void removeBook(Book book){
+        booksList.remove(book);
+    }
+
+    public void displayAllBooks(){
+        System.out.println(booksList);
+    }
+
+
+
+
+}

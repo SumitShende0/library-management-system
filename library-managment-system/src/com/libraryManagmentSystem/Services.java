c7e3361b2700d325a6fa660247fdaf72a1f9d090
Sumit Shende <sumitshende535@gmail.com>
2025-01-19 02:02:18 +0530

    create IllegalArgumentException and BookNotFoundException exception

--- library-managment-system/src/com/libraryManagmentSystem/Services.java
+++ library-managment-system/src/com/libraryManagmentSystem/Services.java
@@ -1,20 +1,27 @@
 package com.libraryManagmentSystem;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Services {
     private static LinkedList<Book> booksList = new LinkedList<>();
+    private static HashMap<Integer, Book> booksMap = new HashMap<>();
 
 
-    public void addBook(Book book){
+    public static void addBook(Book book){
         booksList.add(book);
+        booksMap.put(book.getBookId(), book);
     }
 
-    public void removeBook(Book book){
-        booksList.remove(book);
+    public static void removeBook(int bookId){
+        if (booksMap.containsKey(bookId)){
+            Book bookToRemove = booksMap.remove(bookId);
+            booksList.remove(bookToRemove);
+        }
+
     }
 
-    public void displayAllBooks(){
+    public static void displayAllBooks(){
         System.out.println(booksList);
     }
 
---
+++ library-managment-system/src/com/libraryManagmentSystem/exceptions/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.libraryManagmentSystem.exceptions;
+
+public class IllegalArgumentException extends Exception{
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}

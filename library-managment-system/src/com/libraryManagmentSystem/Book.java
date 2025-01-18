90b077e64122ce3c91ef1f22397330bde9d18702
Sumit Shende <sumitshende535@gmail.com>
2025-01-19 00:28:58 +0530

    book class created

--- library-managment-system/src/com/libraryManagmentSystem/Book.java
+++ library-managment-system/src/com/libraryManagmentSystem/Book.java
@@ -1,4 +1,20 @@
 package com.libraryManagmentSystem;
 
 public class Book {
+    private static int newBookId = 100;
+    private int bookId;
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookAvailabilityStatus;
+
+
+    public Book(String bookTitle, String bookAuthor, String bookAvailabilityStatus) {
+        this.bookId = newBookId++;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookAvailabilityStatus = bookAvailabilityStatus;
+    }
+
+
+
 }
